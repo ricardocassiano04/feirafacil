2c9b16905f91db23898794ffc4c590b09694e922
ricardocassiano04 <rc.cassiano04@gmail.com>
2025-05-08 22:52:45 -0300

    Mudanças nos tipos das chaves primárias e nas referências

--- feirafacil-create.sql
+++ feirafacil-create.sql
@@ -25,69 +25,77 @@ values (0, 'EXCLUIDO'), (1, 'ATIVO');
 
 
 create table produto_categoria (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
-datacadastro timestamp default current_timestamp(0),
-situacao_id integer not null references situacao (id) default 1
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 
 create table produto (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
-id_produtocategoria integer not null references produto_categoria (id),
-datacadastro  timestamp  default current_timestamp(0),
-situacao_id integer not null references situacao (id) default 1
+id_produtocategoria bigint not null references produto_categoria (id),
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro  timestamp  default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 create table item (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
-produto_id integer not null references produto (id),
+produto_id bigint not null references produto (id),
 estoque numeric(13,2),
-datacadastro timestamp  default current_timestamp(0),
 situacao_id integer not null references situacao (id) default 1,
 item_unidade char(2),
-qtde_embalagem integer
+qtde_embalagem integer,
+data_cadastro timestamp  default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 
 create table item_codigo_barras (
-item_id integer not null references item (id),
+item_id bigint not null references item (id),
 codigo_barras numeric(14) not null,
 situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp  default current_timestamp(0),
+data_atualizacao timestamp,
 primary key (item_id, codigo_barras)
 );
 
 
 
 create table lista_compra (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
-datacadastro timestamp default current_timestamp(0),
-situacao_id integer not null references situacao (id) default 1
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
+
 create table lista_compra_item (
-id integer not null primary key generated always as identity,
-lista_compra_id integer not null references lista_compra (id),
-item_id integer not null references item (id),
-datacadastro timestamp default current_timestamp(0),
-situacao_id integer not null references situacao (id) default 1
+id bigserial not null primary key,
+lista_compra_id bigint not null references lista_compra (id),
+item_id bigint not null references item (id),
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 create table pais (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text
 );
 
 create table estado (
-id integer not null primary key ,
-pais_id integer not null references pais (id),
+id bigserial not null primary key,
+pais_id bigint not null references pais (id),
 descricao text,
 sigla char(2)
 );
@@ -95,117 +103,132 @@ sigla char(2)
  
 
 create table cidade (
-id integer not null primary key,
-estado_id integer not null references estado (id),
+id bigserial not null primary key,
+estado_id bigint not null references estado (id),
 descricao text
 );
 
 
 
 create table bairro (
-id integer not null primary key generated always as identity,
-cidade_id integer not null references cidade (id),
-descricao text
+id bigserial not null primary key,
+cidade_id bigint not null references cidade (id),
+descricao text not null,
+unique (cidade_id, descricao)
 );
 
+
+
 create table endereco (
-id integer not null primary key generated always as identity,
-bairro_id integer not null references bairro (id),
+id bigserial not null primary key,
+bairro_id bigint not null references bairro (id),
 endereco text,
-numero integer,
-complementonumero text,
-cep varchar(8)
+cep varchar(8),
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp,
+unique (bairro_id, endereco, cep)
 );
 
 
 create table forma_pagamento (
-id integer not null primary key generated always as identity,
-descricao text,
-situacao_id integer not null references situacao (id) default 1 
+id bigserial not null primary key,
+descricao text UNIQUE,
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 
 create table tipo_estabelecimento (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
-situacao_id integer not null references situacao (id) default 1 
+situacao_id integer not null references situacao (id) default 1,
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 
 
 create table estabelecimento (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 descricao text,
 cnpj varchar(13),
-endereco_id integer not null references endereco (id),
+endereco_id bigint not null references endereco (id),
+endereco_complemento text,
 situacao_id integer not null references situacao (id) default 1,
-tipoestabelecimento_id integer not null references tipo_estabelecimento (id),
-datacadastro timestamp  default current_timestamp(0)
+tipoestabelecimento_id bigint not null references tipo_estabelecimento (id),
+data_cadastro timestamp  default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 create table comprador (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 nome text not null,
 situacao_id integer not null references situacao (id) default 1,
-datacadastro timestamp default current_timestamp(0)
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 create table usuario (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 nome text not null,
 login text not null,
 senha text null,
-endereco_id integer not null references endereco (id),
-comprador_id integer null references comprador (id),
+endereco_id bigint not null references endereco (id),
+endereco_complemento text,
+comprador_id bigint null references comprador (id),
 situacao_id integer not null references situacao (id) default 1,
-datacadastro timestamp default current_timestamp(0)
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 create table terceiro (
-id integer not null primary key generated always as identity,
+id bigserial not null primary key,
 nome text not null,
 situacao_id integer not null references situacao (id) default 1,
-datacadastro timestamp default current_timestamp(0)
+data_cadastro timestamp default current_timestamp(0),
+data_atualizacao timestamp
 );
 
 
 
 
 create table compras (
-id bigint not null primary key generated always as identity,
+id bigserial not null primary key,
 numero_cupom bigint not null,
 numero_nfce bigint,
 compra_data date,
-compra_hora date,
+compra_hora time,
 valor_total numeric(15,2),
 chavenfce varchar(44),
-estabelecimento_id integer references estabelecimento (id),
-usuario_id integer references usuario (id),
+estabelecimento_id bigint references estabelecimento (id),
+usuario_id bigint references usuario (id),
 datahorainclusao timestamp default current_timestamp(0)
 );
 
 
 create table compras_forma_pagamento (
-id bigint not null primary key generated always as identity,
+id bigserial not null primary key,
 compra_id bigint references compras (id),
-forma_pagamento_id integer references forma_pagamento (id),
+forma_pagamento_id bigint references forma_pagamento (id),
 unique (compra_id, forma_pagamento_id)
 );
 
 
 
 create table compras_item (
-id bigint not null primary key generated always as identity,
+id bigserial not null primary key,
 compras_id bigint references compras (id),
 posicao_item_cupom integer,
 codigo_item_estabelecimento integer,
-item_id integer references item (id),
+item_id bigint references item (id),
 quantidade numeric(15,2) not null,
 preco_unitario numeric(15,2),
-valor_total_item numeric(15,2)
+valor_total_item numeric(15,2),
+datahorainclusao timestamp default current_timestamp(0)
 );
